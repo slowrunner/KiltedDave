72617c45cf28dced413288dc9831603247720cbf
slowrunner <slowrunner@users.noreply.github.com>
2026-04-25 09:08:55 -0400

    Fix activation message and improve venv checks

--- systests/moondream/activate_006.sh
+++ systests/moondream/activate_006.sh
@@ -1,17 +1,20 @@
 #!/bin/bash 
 # Set the path to your virtual environment directory
+# (may need to be complete path)
 VENV_PATH="./moondream_006_venv" 
 
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    echo "!!! Must use:  . activate_006_venv.sh"
-    exit 0
-fi
+    echo "!!! Must use:  . activate_venv.sh"
 
-if [ -d "$VENV_PATH" ] && [ -f "$VENV_PATH/bin/activate" ]; then
-    echo "Activating virtual environment..."
+
+elif [ "$VIRTUAL_ENV" == "$VENV_PATH" ]; then
+    echo "venv already active"
+
+elif [ -f "$VENV_PATH/bin/activate" ] ; then
+    echo "Activating virtual environment...source $VENV_PATH/bin/activate" 
     source "$VENV_PATH/bin/activate"
+    echo "type deactivate to exit venv"
 else
     echo "Error: Virtual environment not found at $VENV_PATH"
-    exit 1
 fi
