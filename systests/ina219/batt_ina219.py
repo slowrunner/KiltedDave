d7cea2f546b25f99063fd4b632ef073f1296e2fa
slowrunner <slowrunner@users.noreply.github.com>
2026-04-26 23:47:56 -0400

    Add batt_ina219.py for INA219 sensor readings
    Implement INA219 battery monitoring script.

---
+++ systests/ina219/batt_ina219.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from ina219 import INA219
+from ina219 import DeviceRangeError
+import inspect
+
+SHUNT_OHMS = 0.1
+
+print(inspect.signature(INA219))
+
+def read():
+    ina = INA219(SHUNT_OHMS,busnum=1)
+    ina.configure()
+
+    print("Bus Voltage: %.3f V" % ina.voltage())
+    try:
+        print("Bus Current: %.3f mA" % ina.current())
+        print("Power: %.3f mW" % ina.power())
+        print("Shunt voltage: %.3f mV" % ina.shunt_voltage())
+    except DeviceRangeError as e:
+        # Current out of device range with specified shunt resistor
+        print(e)
+
+
+if __name__ == "__main__":
+    read()
